9792f502417e9487d413dcff7b0538131d99300f
northyde <northyde@yahoo.de>
2018-01-08 23:03:00 +0100

    style
    new formatting, background

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     // Adds 1 goal for team 1 by clicking the goal button
